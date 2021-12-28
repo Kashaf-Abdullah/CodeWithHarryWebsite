2c93691b16225430ea61efeea5ae7fd4b40cb534
Kashaf Abdullah <90180384+Kashaf-Abdullah@users.noreply.github.com>
2021-12-28 14:52:55 +0500

    Add files via upload

---
+++ js_2web10.js
@@ -0,0 +1,97 @@
+function toggleMobileMenu(menu){
+    menu.classList.toggle('open');
+}
+function freewatch1(){
+    window.open("https://youtu.be/-mJFZp84TIY");
+}
+function freewatch2(){
+    window.open("https://youtu.be/5_5oE5lgrhw");
+}
+function freewatch3(){
+    window.open("https://youtu.be/7Dh73z3icd8");
+}
+
+
+function freewatch4(){
+    window.open("https://youtu.be/vrQlEhmVFDA");
+}
+
+
+function freewatch5(){
+    window.open("https://youtu.be/ntLJmHOJ0ME");
+}
+
+
+function freewatch6(){
+    window.open("https://youtu.be/InigFUSiPl8");
+}
+function freewatch7(){
+    window.open("https://youtu.be/j8nAHeVKL08");
+}
+function freewatch8(){
+    window.open("https://youtu.be/6mbwJ2xhgzM");
+}
+function freewatch9(){
+    window.open("https://youtu.be/5BDgKJFZMl8");
+}
+function freewatch10(){
+    window.open("https://youtu.be/vrQlEhmVFDA");
+}
+function freewatch11(){
+    window.open("https://youtu.be/2rMkZhwyJGY");
+}
+
+function freewatch12(){
+    window.open("https://youtu.be/DD3ou9sa3Z8");
+}
+
+function freewatch13(){
+    window.open("https://youtu.be/qHJjMvHLJdg");
+}
+
+function freewatch14(){
+    window.open("https://youtu.be/at19OmH2Bg4");
+}
+
+function freewatch15(){
+    window.open("https://youtu.be/vunhMBRfhL8");
+}
+
+function freewatch16(){
+    window.open("https://youtu.be/cvvwkgp4HBg");
+}
+
+
+function freewatch17(){
+    window.open("https://youtu.be/pF-h1YCS5GE");
+}
+function freewatch18(){
+    window.open("https://youtu.be/qzbZIxnFKPc");
+}
+function freewatch19(){
+    window.open("https://youtu.be/evknSAkUIvs");
+}
+function freewatch20(){
+    window.open("https://youtu.be/unYqUyeGWQY");
+}
+function freewatch21(){
+    window.open("https://youtu.be/eXudjwOwv-8");
+}
+function freewatch22(){
+    window.open("https://youtu.be/3zBhmYSLB70");
+}
+function freewatch23(){
+    window.open("https://youtu.be/fpdwRofNMeQ");
+}
+function freewatch24(){
+    window.open("https://youtu.be/_JWUPnq2g-8");
+}
+function freewatch25(){
+    window.open("https://youtu.be/ADiRLqaGMy8");
+}
+function freewatch26(){
+    window.open("https://youtu.be/xdj0mGmuNjc");
+}
+function freewatch27(){
+    window.open("https://youtu.be/9-GSIN8s5Eo");
+}
---
+++ js_web10.js
@@ -0,0 +1,14 @@
+function toggleMobileMenu(menu){
+    menu.classList.toggle('open');
+}
+
+
+function watch1(){
+window.open("https://youtu.be/5_5oE5lgrhw");
+}
+function watch2(){
+    window.open("https://youtu.be/-mJFZp84TIY");
+}
+function watch3(){
+    window.open("https://youtu.be/-mJFZp84TIY")
+}
